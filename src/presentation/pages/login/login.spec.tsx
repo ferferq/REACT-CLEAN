b53a61d3027457e3cf57b1cb879ddfa6f95508f7
ferferq <fernandoalvesq@gmail.com>
2022-06-05 15:27:53 -0300

    test: prevent Login to call submit more than once

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -180,4 +180,15 @@ describe('Login Component', () => {
       password,
     });
   });
+
+  test('Should call Authentication only once', () => {
+    const { sut, authenticationSpy } = makeSut();
+    simulateValidSubmit({
+      sut,
+    });
+    simulateValidSubmit({
+      sut,
+    });
+    expect(authenticationSpy.callsCount).toBe(1);
+  });
 });
