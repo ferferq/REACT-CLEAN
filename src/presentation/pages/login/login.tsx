b1591169b8916b27f7df2d3f906d048687389d35
ferferq <fernandoalvesq@gmail.com>
2022-06-05 09:58:43 -0300

    test: test

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -23,7 +23,7 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
     email: '',
     password: '',
     emailError: '',
-    passwordError: '',
+    passwordError: 'teste',
     mainError: '',
   });
 
