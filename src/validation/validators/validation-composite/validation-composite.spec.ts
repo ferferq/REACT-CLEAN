3681341a9a27dcfb711f0c436e8d79edbc30b9d6
ferferq <fernandoalvesq@gmail.com>
2022-06-18 11:43:16 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,16 +1,30 @@
 import { FieldValidationSpy } from '../test';
 import { ValidationComposite } from './validation-composite';
 
+type SutTypes = {
+  sut: ValidationComposite;
+  fieldValidationsSpy: FieldValidationSpy[];
+};
+
+const makeSut = (): SutTypes => {
+  const fieldValidationsSpy = [
+    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy('any_field'),
+  ];
+  const sut = new ValidationComposite(fieldValidationsSpy);
+
+  return {
+    sut,
+    fieldValidationsSpy,
+  };
+};
+
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field');
+    const { sut, fieldValidationsSpy } = makeSut();
+    const [fieldValidationSpy, fieldValidationSpy2] = fieldValidationsSpy;
     fieldValidationSpy.error = new Error('first_message');
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field');
     fieldValidationSpy2.error = new Error('secound_message');
-    const sut = new ValidationComposite([
-      fieldValidationSpy,
-      fieldValidationSpy2,
-    ]);
     const error = sut.validate('any_field', 'any_value');
     expect(error).toBe('first_message');
   });
