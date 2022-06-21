5767040a1b756a94f882c23d23bbdd2bee074a22
ferferq <fernandoalvesq@gmail.com>
2022-06-20 22:26:02 -0300

    refactor: move validation test folder to correct place

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,5 +1,5 @@
 import faker from 'faker';
-import { FieldValidationSpy } from '../test';
+import { FieldValidationSpy } from '@/validation/test';
 import { ValidationComposite } from './validation-composite';
 
 type SutTypes = {
