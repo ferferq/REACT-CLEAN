ed90754e281cd43e0de93e67594cbff20dc9a3a8
ferferq <fernandoalvesq@gmail.com>
2022-06-20 22:44:49 -0300

    feat: add Login composition

--- src/presentation/pages/app/app.tsx
+++ src/presentation/pages/app/app.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Router } from '@/infra/router';
 import '@/presentation/styles/global.scss';
+import { makeLogin } from '@/main/factories/pages/login/login-factory';
 
 export const App: React.FC = () => {
-  return <Router />;
+  return <Router MakeLogin={makeLogin} />;
 };
