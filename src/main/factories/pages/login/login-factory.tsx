8a72732f95729f70abdd3750d0fa4f336b62f727
ferferq <fernandoalvesq@gmail.com>
2022-06-22 13:10:04 -0300

    refactor: change index file recive pages

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Login } from '@/presentation/pages';
+import { Login } from '@/presentation/pages/login';
 import { makeRemoteAuthentication } from '@/main/factories/useCases/authentication/remote-authentication-factory';
 import { makeLoginValidation } from './login-validation-factory';
 
