72eddde574359bf621ad622719b2797e40cc4f67
ferferq <fernandoalvesq@gmail.com>
2022-06-23 19:03:21 -0300

    test: test travis

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -15,7 +15,6 @@ export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   async post(params: HttpPostParams<T>): Promise<HttpResponse<R>> {
     this.url = params.url;
     this.body = params.body;
-
     return this.response;
   }
 }
