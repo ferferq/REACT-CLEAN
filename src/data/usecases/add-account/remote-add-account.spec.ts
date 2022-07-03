4e8755aacd3648810585e27770c6adfb450f3454
ferferq <fernandoalvesq@gmail.com>
2022-07-03 16:16:18 -0300

    test: ensure RemoteAddAccount return an AccoutnModel on success

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -2,7 +2,7 @@ import { HttpStatusCode } from '@/data/protocols/http';
 import { HttpPostClientSpy } from '@/data/test';
 import { EmailInUseError, UnexpectedError } from '@/domain/errors';
 import { AccountModel } from '@/domain/models';
-import { mockAddAccount } from '@/domain/test';
+import { mockAccountModel, mockAddAccount } from '@/domain/test';
 import { AddAccountParams } from '@/domain/usecases';
 import faker from 'faker';
 import { RemoteAddAccount } from './remote-add-account';
@@ -77,5 +77,17 @@ describe('RemoteAuthentication', () => {
 
     const promise = sut.add(mockAddAccount());
     await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test('Should throw AccountModel if HttpPostClient returns 200', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    const httpResult = mockAccountModel();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult,
+    };
+
+    const account = await sut.add(mockAddAccount());
+    expect(account).toEqual(httpResult);
   });
 });
