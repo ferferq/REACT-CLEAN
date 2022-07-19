e16dc4423205ac2c8a1f2608355b4cfbbc089af5
ferferq <fernandoalvesq@gmail.com>
2022-07-19 09:02:15 -0300

    test: ensure login present error message if invalid credentials on error 401

--- src/main/test/cypress/integration/pages/login.spec.ts
+++ src/main/test/cypress/integration/pages/login.spec.ts
@@ -45,36 +45,27 @@ describe('Login', () => {
     cy.getByTestId('error-wrap').should('not.have.descendants');
   });
 
-  it('Should present error if invalid credentials are provided', () => {
+  it('Should present InvalidCredentialsError on 401', () => {
+    cy.intercept(
+      {
+        method: 'POST',
+        url: /login/,
+      },
+      {
+        statusCode: 401,
+        body: {
+          error: faker.random.words(),
+        },
+      },
+    );
     cy.getByTestId('email').focus().type(faker.internet.email());
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5));
     cy.getByTestId('submit').click();
-    cy.getByTestId('error-wrap')
-      .getByTestId('spinner')
-      .should('exist')
-      .getByTestId('main-error')
-      .should('not.exist')
-      .getByTestId('spinner')
-      .should('not.exist')
-      .getByTestId('main-error')
-      .should('contain.text', 'Credentials inválidas');
-    cy.url().should('eq', `${baseUrl}/login`);
-  });
-
-  it('Should present save accessToken if valid credentials are provided', () => {
-    cy.getByTestId('email').focus().type('mango@gmail.com');
-    cy.getByTestId('password').focus().type('12345');
-    cy.getByTestId('submit').click();
-    cy.getByTestId('error-wrap')
-      .getByTestId('spinner')
-      .should('exist')
-      .getByTestId('main-error')
-      .should('not.exist')
-      .getByTestId('spinner')
-      .should('not.exist');
-    cy.url().should('eq', `${baseUrl}/`);
-    cy.window().then((window) =>
-      assert.isOk(window.localStorage.getItem('accessToken')),
+    cy.getByTestId('spinner').should('not.exist');
+    cy.getByTestId('main-error').should(
+      'contain.text',
+      'Credentials inválidas',
     );
+    cy.url().should('eq', `${baseUrl}/login`);
   });
 });
