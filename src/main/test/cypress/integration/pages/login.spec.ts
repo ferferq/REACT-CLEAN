cf005b3fcb7f565107bbdaad4b598e4e94bdda01
ferferq <fernandoalvesq@gmail.com>
2022-07-19 09:02:54 -0300

    test: ensure login present error message if unexpected error  on error 400

--- src/main/test/cypress/integration/pages/login.spec.ts
+++ src/main/test/cypress/integration/pages/login.spec.ts
@@ -68,4 +68,28 @@ describe('Login', () => {
     );
     cy.url().should('eq', `${baseUrl}/login`);
   });
+
+  it('Should present UnexpectedError on 400', () => {
+    cy.intercept(
+      {
+        method: 'POST',
+        url: /login/,
+      },
+      {
+        statusCode: 400,
+        body: {
+          error: faker.random.words(),
+        },
+      },
+    );
+    cy.getByTestId('email').focus().type(faker.internet.email());
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5));
+    cy.getByTestId('submit').click();
+    cy.getByTestId('spinner').should('not.exist');
+    cy.getByTestId('main-error').should(
+      'contain.text',
+      'Algo de errado aconteceu. Tente novamente em breve.',
+    );
+    cy.url().should('eq', `${baseUrl}/login`);
+  });
 });
