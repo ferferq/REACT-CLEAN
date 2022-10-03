a061eefb9d8ced0ec39d74dce757ac2ca61efe20
ferferq <fernandoalvesq@gmail.com>
2022-10-03 08:44:50 -0300

    feat: ensure SurveyList render error on failure

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -16,27 +16,38 @@ type Props = {
 const SurveyList: React.FC<Props> = ({ loadSurveyList }) => {
   const [state, setState] = useState({
     surveys: [] as SurveyModel[],
+    error: '',
   });
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    loadSurveyList.loadAll().then((surveys) => {
-      setState({ surveys });
-    });
+    loadSurveyList
+      .loadAll()
+      .then((surveys) => {
+        setState({ ...state, surveys });
+      })
+      .catch((error) => setState({ ...state, error: error.message }));
   }, []);
   return (
     <div className={Styles.surveyListWrap}>
       <Header />
       <main className={Styles.contentWrap}>
         <h2>Enquetes</h2>
-        <ul data-testid="survey-list">
-          {state.surveys.length ? (
-            state.surveys.map((survey: SurveyModel) => (
-              <SurveyItem key={survey.id} survey={survey} />
-            ))
-          ) : (
-            <SurveyItemEmpty />
-          )}
-        </ul>
+        {state.error ? (
+          <div>
+            <span data-testid="error">{state.error}</span>
+            <button>Recarregar</button>
+          </div>
+        ) : (
+          <ul data-testid="survey-list">
+            {state.surveys.length ? (
+              state.surveys.map((survey: SurveyModel) => (
+                <SurveyItem key={survey.id} survey={survey} />
+              ))
+            ) : (
+              <SurveyItemEmpty />
+            )}
+          </ul>
+        )}
       </main>
       <Footer />
     </div>
