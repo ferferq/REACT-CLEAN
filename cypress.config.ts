79f1fa99d53f155830ba9f019a68e663a2283bcf
ferferq <fernandoalvesq@gmail.com>
2023-01-21 18:35:04 -0300

    chore: config travis ci to use cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     baseUrl: 'http://localhost:8080',
     fixturesFolder: false,
     supportFile: 'src/main/test/cypress/support/index.js',
-    specPattern: 'src/main/test/cypress/integration/**/*.spec.{ts,tsx}'
+    specPattern: 'src/main/test/cypress/integration/**/*.spec.{ts,tsx}',
+    video: false,
   },
 });
